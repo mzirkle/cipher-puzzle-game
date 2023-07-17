06fba4b2ee26d2616d2b42dcfce79033e5e76928
Michael Zirkle <30888411+mzirkle@users.noreply.github.com>
2023-07-16 19:18:51 -0700

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 let secretPhrase = ''; // Stores the secret phrase
 let rows = []; // Stores the rows of the puzzle
-let currentRow = 0; // Index of the current row
-let currentCell = 0; // Index of the current cell in the row
+let columns = []; // Stores the columns of the puzzle
 let isSolved = false; // Flag to check if the puzzle is solved
+let selectedCell = null; // Reference to the currently selected cell
 
 const puzzleContainer = document.getElementById('puzzle-container');
-const inputBox = document.getElementById('input-box');
-const submitButton = document.getElementById('submit-button');
 const resultText = document.getElementById('result-text');
 
 initializeGame();
 
-submitButton.addEventListener('click', handleInput);
+puzzleContainer.addEventListener('click', handleCellClick);
+document.addEventListener('keydown', handleKeyPress);
 
 function initializeGame() {
   secretPhrase = generateSecretPhrase();
-  rows = [];
-  currentRow = 0;
-  currentCell = 0;
+  rows = secretPhrase.split(' ');
+  columns = [];
   isSolved = false;
   resultText.textContent = '';
 
-  const words = secretPhrase.split(' ');
+  const maxRowLength = Math.max(...rows.map(row => row.length));
 
-  words.forEach(word => {
-    const row = document.createElement('div');
-    row.className = 'row';
+  for (let col = 0; col < maxRowLength; col++) {
+    const column = [];
+    for (let row = 0; row < rows.length; row++) {
+      const letter = row < rows.length && col < rows[row].length ? rows[row][col] : '';
+      column.push(letter);
+    }
+    columns.push(column);
+  }
 
-    for (let i = 0; i < word.length; i++) {
-      const cell = document.createElement('div');
-      cell.className = 'input-cell';
-      row.appendChild(cell);
+  for (let row = 0; row < rows.length; row++) {
+    const rowElement = document.createElement('div');
+    rowElement.className = 'row';
+
+    for (let col = 0; col < columns.length; col++) {
+      const letter = columns[col][row];
+      const cellElement = document.createElement('div');
+      cellElement.className = 'cell';
+      cellElement.textContent = letter;
+      cellElement.setAttribute('data-row', row);
+      cellElement.setAttribute('data-col', col);
+      rowElement.appendChild(cellElement);
     }
 
-    rows.push(row);
-    puzzleContainer.appendChild(row);
-  });
+    puzzleContainer.appendChild(rowElement);
+  }
 }
 
 function generateSecretPhrase() {
@@ -43,38 +53,34 @@ function generateSecretPhrase() {
   return phrases[Math.floor(Math.random() * phrases.length)].toUpperCase();
 }
 
-function handleInput() {
+function handleCellClick(event) {
   if (isSolved) {
     resultText.textContent = 'Puzzle already solved.';
     return;
   }
 
-  const input = inputBox.value.toUpperCase();
-
-  if (input.length !== 1) {
-    resultText.textContent = 'Please enter a single letter.';
-    return;
-  }
-
-  const currentCellElement = rows[currentRow].children[currentCell];
-  currentCellElement.textContent = input;
-
-  if (currentCell === rows[currentRow].children.length - 1) {
-    currentRow++;
-    currentCell = 0;
+  const cell = event.target;
+  if (cell.className === 'cell') {
+    selectedCell = cell;
+    cell.classList.add('selected');
   } else {
-    currentCell++;
+    selectedCell = null;
+    document.querySelectorAll('.cell.selected').forEach(el => el.classList.remove('selected'));
   }
+}
 
-  if (currentRow === rows.length) {
+function handleKeyPress(event) {
+  if (selectedCell && event.key.match(/^[a-z]$/i)) {
+    selectedCell.textContent = event.key.toUpperCase();
+    const row = parseInt(selectedCell.getAttribute('data-row'), 10);
+    const col = parseInt(selectedCell.getAttribute('data-col'), 10);
+    columns[col][row] = event.key.toUpperCase();
     checkSolution();
   }
-
-  inputBox.value = '';
 }
 
 function checkSolution() {
-  const solution = rows.map(row => Array.from(row.children).map(cell => cell.textContent).join('')).join(' ');
+  const solution = columns.map(column => column.join('')).join(' ');
   if (solution === secretPhrase) {
     isSolved = true;
     resultText.textContent = 'Congratulations! You solved the puzzle!';
