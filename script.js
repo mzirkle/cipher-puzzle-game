c9f80aa1f16332d4883402ddb3e20954db524b8a
Michael Zirkle <30888411+mzirkle@users.noreply.github.com>
2023-07-16 19:13:19 -0700

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 let secretPhrase = ''; // Stores the secret phrase
-let skip = 0; // Number of positions to skip
-let attempts = 0; // Number of attempts made
+let rows = []; // Stores the rows of the puzzle
+let currentRow = 0; // Index of the current row
+let currentCell = 0; // Index of the current cell in the row
+let isSolved = false; // Flag to check if the puzzle is solved
 
-const puzzleText = document.getElementById('puzzle-text');
-const guessInput = document.getElementById('guess-input');
+const puzzleContainer = document.getElementById('puzzle-container');
+const inputBox = document.getElementById('input-box');
 const submitButton = document.getElementById('submit-button');
 const resultText = document.getElementById('result-text');
 
 initializeGame();
 
-submitButton.addEventListener('click', checkGuess);
+submitButton.addEventListener('click', handleInput);
 
 function initializeGame() {
   secretPhrase = generateSecretPhrase();
-  skip = Math.floor(Math.random() * 10) + 1; // Generate a skip value between 1 and 10
-  attempts = 0;
-  puzzleText.textContent = generatePuzzleText(secretPhrase, skip);
-  guessInput.value = '';
+  rows = [];
+  currentRow = 0;
+  currentCell = 0;
+  isSolved = false;
   resultText.textContent = '';
+
+  const words = secretPhrase.split(' ');
+
+  words.forEach(word => {
+    const row = document.createElement('div');
+    row.className = 'row';
+
+    for (let i = 0; i < word.length; i++) {
+      const cell = document.createElement('div');
+      cell.className = 'input-cell';
+      row.appendChild(cell);
+    }
+
+    rows.push(row);
+    puzzleContainer.appendChild(row);
+  });
 }
 
 function generateSecretPhrase() {
@@ -25,35 +43,42 @@ function generateSecretPhrase() {
   return phrases[Math.floor(Math.random() * phrases.length)].toUpperCase();
 }
 
-function generatePuzzleText(phrase, skip) {
-  let puzzle = '';
-  for (let i = 0; i < phrase.length; i++) {
-    if (phrase[i] === ' ') {
-      puzzle += ' '; // Preserve spaces in the puzzle
-    } else if ((i + 1) % skip === 0) {
-      puzzle += phrase[i];
-    } else {
-      puzzle += '_';
-    }
-  }
-  return puzzle;
-}
-
-function checkGuess() {
-  const guess = guessInput.value.toUpperCase();
-  attempts++;
-
-  if (guess.length !== secretPhrase.length) {
-    resultText.textContent = 'Invalid guess length';
+function handleInput() {
+  if (isSolved) {
+    resultText.textContent = 'Puzzle already solved.';
     return;
   }
 
-  if (guess === secretPhrase) {
-    resultText.textContent = 'Congratulations! You solved the puzzle!';
-    submitButton.disabled = true;
-  } else {
-    resultText.textContent = 'Incorrect guess, try again.';
+  const input = inputBox.value.toUpperCase();
+
+  if (input.length !== 1) {
+    resultText.textContent = 'Please enter a single letter.';
+    return;
   }
 
-  guessInput.value = '';
+  const currentCellElement = rows[currentRow].children[currentCell];
+  currentCellElement.textContent = input;
+
+  if (currentCell === rows[currentRow].children.length - 1) {
+    currentRow++;
+    currentCell = 0;
+  } else {
+    currentCell++;
+  }
+
+  if (currentRow === rows.length) {
+    checkSolution();
+  }
+
+  inputBox.value = '';
+}
+
+function checkSolution() {
+  const solution = rows.map(row => Array.from(row.children).map(cell => cell.textContent).join('')).join(' ');
+  if (solution === secretPhrase) {
+    isSolved = true;
+    resultText.textContent = 'Congratulations! You solved the puzzle!';
+  } else {
+    resultText.textContent = 'Incorrect solution. Please try again.';
+  }
 }
